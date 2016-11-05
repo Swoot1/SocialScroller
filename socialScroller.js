d467d73d48c70f3d55c891329cc3e0cc3064ecb3
elinnilsson <elinhejnilsson@gmail.com>
2016-11-05 21:26:47 +0100

    Added function for starting and stopping.

--- socialScroller.js
+++ socialScroller.js
@@ -1,15 +1,23 @@
-var minutesOfScrolling = 10;
-var readingPauseInMilliseconds = 2500;
-var startTime = Date.now();
-var endTime = startTime + 1000*60*minutesOfScrolling;
+window.SocialScrolling = {
+    intervalId: null,
+    startScrolling: function (minutesOfScrolling, readingPauseInMilliseconds){
+        var startTime = Date.now();
+        var endTime = startTime + 1000*60*minutesOfScrolling;
+        minutesOfScrolling = minutesOfScrolling || 10;
+        readingPauseInMilliseconds = readingPauseInMilliseconds || 2500;
 
-var intervalId = window.setInterval(() => {
-    var timeToStopScrolling = Date.now() > endTime;
-    
-    if(timeToStopScrolling){
-        clearInterval(intervalId);
+        this.intervalId = window.setInterval(() => {
+            var timeToStopScrolling = Date.now() > endTime;
+
+            if(timeToStopScrolling){
+                this.stopScrolling();
+            }
+
+            window.scrollTo(0,document.body.scrollHeight);
+
+        }, readingPauseInMilliseconds);
+    },
+    stopScrolling: function(){
+        clearInterval(this.intervalId);
     }
-    
-    window.scrollTo(0,document.body.scrollHeight);
-    
-}, readingPauseInMilliseconds);
+};
